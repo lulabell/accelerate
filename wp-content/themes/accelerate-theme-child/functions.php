a7eb4b18d860e6203f8dc428a11bd72a0386be43
lulabell <bell_grace@hotmail.com>
2017-02-02 22:42:45 -0500

    created custom wp login styles in function.php and added images

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -67,6 +67,68 @@ function accelerate_theme_child_widget_init() {
 }
 add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
 
+//link to mobile.css - may want to delete and add media queries to bottom of style.css instead
+function custom_style_sheet() {
+wp_enqueue_style( 'mobile', get_stylesheet_directory_uri() . '/mobile.css' );
+}
+add_action('wp_enqueue_scripts', 'custom_style_sheet');
+
+//change login logo, background
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/accelerate-logo.png);
+            padding-bottom: 30px;
+        }
+		#login h1 a:hover, .login h1 a:hover {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/accelerate-reverse-logo.png);
+		}
+		body.login { background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/taxi-photo.jpg);
+		-webkit-background-size: cover;
+ 		-moz-background-size: cover;
+		-o-background-size: cover;
+		 background-size: cover;
+		 position:fixed;
+		 top:0;
+		 left:0;
+		 z-index:10;
+		 overflow: hidden;
+		 width: 100%;
+		 height:100%;
+		}
+		body.login div#login p#nav a,
+body.login div#login p#backtoblog a {
+    color: #45ac9d !important; /* Accelerate link color. */
+		}
+		body.login div#login p#nav a:hover,
+body.login div#login p#backtoblog a:hover {
+    color: #fff !important; /* Accelerate link color. */
+		}
+		
+		.login form { background-color: rgb(69, 172, 157, 2); }
+		
+		
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+//change logo link value from WP site to accelerate site
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
 
 
 
+
+
+
+
+
+
+//
